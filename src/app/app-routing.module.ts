c65c20f7e4e806f47f6a7926bb6c45ddb32ac549
Alexandra Severiukhina <aseveriukhina@griddynamics.com>
2019-02-18 14:10:08 +0400

    draft of components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {CartComponent} from './cart/cart.component';
+import {DefaultComponent} from './default/default.component';
+import {ProductListComponent} from './product-list/product-list.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: ProductListComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '404', component: DefaultComponent },
+  {path: '**', redirectTo: '/404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
+import { DetailsComponent } from './details/details.component';
+import { DefaultComponent } from './default/default.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductListComponent,
+    ProductComponent,
+    DetailsComponent,
+    DefaultComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
